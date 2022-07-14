9ae2d66894bbe7ac4197c0cb8f30d6112e103213
PD <cosmos.lab.contact@gmail.com>
2022-07-14 23:58:49 +0530

    Refactor

--- shopping-cart/src/components/Loading/Spinner/index.tsx
+++
@@ -1,8 +0,0 @@
-import css from "classnames";
-
-import "./styles.scss";
-function Spinner(props: any) {
-  return <div className={css("spinner", props.className)}></div>;
-}
-
-export default Spinner;
